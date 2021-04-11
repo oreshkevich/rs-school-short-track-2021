e8d0a239a48ace32d3cbac4e21a860faf1b8b0f8
Henadzi <68184678+oreshkevich@users.noreply.github.com>
2021-04-11 22:38:18 +0300

    Update 11-encode-line.js

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,18 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  let x = 1;
+  let r = '';
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === str[i + 1]) {
+      x += 1;
+    } else {
+      r += x + str[i];
+      x = 1;
+    }
+  }
+  return r.replace(/1/g, '');
 }
 
 module.exports = encodeLine;

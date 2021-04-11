13e13564b5ccb71671257974eb03758fa9e38f21
Henadzi <68184678+oreshkevich@users.noreply.github.com>
2021-04-11 23:11:32 +0300

    Update 15-get-email-domain.js

--- src/15-get-email-domain.js
+++ src/15-get-email-domain.js
@@ -8,8 +8,17 @@
  * For the input 'prettyandsimple@example.com', the output should be 'example.com'
  *
  */
-function getEmailDomain(/* email */) {
-  throw new Error('Not implemented');
+function getEmailDomain(email) {
+  const arr = email.split('');
+  let k = '';
+  let address = '';
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].charCodeAt(0) === 64) {
+      k = arr.slice(i + 1);
+    }
+  }
+  address = k.join('');
+  return address;
 }
 
 module.exports = getEmailDomain;
